e1e50220b4c05592b3bd8b7ea8a6aa707fa24405
boomerangbro12 <boomerangbro12@gmail.com>
2019-12-28 10:47:06 -0500

    First use of git for FTC11525!

---
+++ src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Main.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Autonomous_Main extends LinearOpMode {
+    //WE GOT GIT NOW WE PRO TEAM
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
